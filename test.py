e249f032b77847bcb410f27f5537e28a1c9ed96e
sim manseop <tlqaksqhr@naver.com>
2020-02-20 23:12:43 +0900

    add test code

---
+++ test.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import megabox
+
+data = megabox.getBookingMovieListIDByDate("20200219")
+
+users_tag = [
+	{
+	'user' : 'Alex',
+	'tags' : [
+		'러브라이브 선샤인',
+		'First LOVELIVE',
+	]},
+	{
+	'user' : 'Kim',
+	'tags' : [
+		'First LOVELIVE',
+		'러브라이브 선샤인',
+	]},
+]
+
+
+addMovieDate()
+
+'''
+
+param
+
+user_tag = {
+	'user' : 'xxxx',
+	'tags' : [
+		'yyyyx',
+		'yyyyy',
+		...
+	]
+}
+
+return_value
+
+user_value = [
+	{
+		'user' : 'xxxxxx',
+		'search_result' : [
+			{'movieNo': '20004000', 'movieNm': 'yyyyy'}
+		]
+	},
+	{
+		'user' : 'xxxxxy',
+		'search_result' : [
+			{'movieNo': '20004000', 'movieNm': 'yyyyy'}
+		]
+	},
+	...
+]
+
+'''
